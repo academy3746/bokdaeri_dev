51c7a17c2883a9f8faa4470b9cfda62e8cfc4088
ijonghyeon <academy3746@gmail.com>
2023-06-09 11:46:38 +0900

    2. 2차 Raw

---
+++ lib/features/webview_controller.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WebviewController extends StatefulWidget {
+  const WebviewController({super.key});
+
+  @override
+  State<WebviewController> createState() => _WebviewControllerState();
+}
+
+class _WebviewControllerState extends State<WebviewController> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
